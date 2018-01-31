9e0381bf845b5476d1c936b1d04abc31f4c641e1
Pablo Vallejos <p.vallejos86@gmail.com>
2018-01-31 16:13:59 -0300

    init

---
+++ wp-content/themes/mesmerize/template-parts/footer/footer.php
@@ -0,0 +1,20 @@
+<div <?php echo mesmerize_footer_container('footer-simple') ?>>
+    <div <?php echo mesmerize_footer_background('footer-content center-xs') ?>>
+        <div class="gridContainer">
+	        <div class="row middle-xs footer-content-row">
+	            <div class="footer-content-col col-xs-12">
+	                    <div class="cipre-ftr">
+							<img src="http://localhost/wp-content/uploads/2018/01/cropped-mainlogowt.png">
+							<div class="col-cont">
+								<span class="phone"><i class="fa fa-phone"></i>+56 2 27536410</span>
+								<span class="mail">
+									<a href="mailto:contacto@cipresillos.cl?Subject=contacto%20web" target="_top"><i class="fa fa-envelope"></i>contacto@cipresillos.cl</a>
+								</span>
+							</div>
+						</div>
+					</div>
+	            </div>
+	        </div>
+	    </div>
+    </div>
+</div>
